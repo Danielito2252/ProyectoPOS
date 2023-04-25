1167cb17ab6546a977f0181dd4e6d4ff0cdc7deb
Herberth Barrios <hbarriosb4@miumg.edu.gt>
2023-04-24 23:15:10 -0600

    Se agrega el proyecto

---
+++ CapaPresentacion/Inicio.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace CapaPresentacion
+{
+    partial class frmInicio
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuTitulo = new System.Windows.Forms.MenuStrip();
+            this.iconMenuItem3 = new FontAwesome.Sharp.IconMenuItem();
+            this.iconMenuItem1 = new FontAwesome.Sharp.IconMenuItem();
+            this.iconMenuItem2 = new FontAwesome.Sharp.IconMenuItem();
+            this.iconMenuItem4 = new FontAwesome.Sharp.IconMenuItem();
+            this.iconMenuItem5 = new FontAwesome.Sharp.IconMenuItem();
+            this.iconMenuItem6 = new FontAwesome.Sharp.IconMenuItem();
+            this.iconMenuItem7 = new FontAwesome.Sharp.IconMenuItem();
+            this.menuIconos = new System.Windows.Forms.MenuStrip();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.menuTitulo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuTitulo
+            // 
+            this.menuTitulo.Dock = System.Windows.Forms.DockStyle.Left;
+            this.menuTitulo.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.iconMenuItem3,
+            this.iconMenuItem1,
+            this.iconMenuItem2,
+            this.iconMenuItem4,
+            this.iconMenuItem5,
+            this.iconMenuItem6,
+            this.iconMenuItem7});
+            this.menuTitulo.Location = new System.Drawing.Point(0, 24);
+            this.menuTitulo.Name = "menuTitulo";
+            this.menuTitulo.Size = new System.Drawing.Size(106, 420);
+            this.menuTitulo.TabIndex = 0;
+            this.menuTitulo.Text = "menuTitulo";
+            // 
+            // iconMenuItem3
+            // 
+            this.iconMenuItem3.AutoSize = false;
+            this.iconMenuItem3.BackColor = System.Drawing.Color.Honeydew;
+            this.iconMenuItem3.IconChar = FontAwesome.Sharp.IconChar.Shop;
+            this.iconMenuItem3.IconColor = System.Drawing.Color.Black;
+            this.iconMenuItem3.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconMenuItem3.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.iconMenuItem3.Name = "iconMenuItem3";
+            this.iconMenuItem3.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.iconMenuItem3.Size = new System.Drawing.Size(100, 75);
+            this.iconMenuItem3.Text = "Compras";
+            this.iconMenuItem3.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // iconMenuItem1
+            // 
+            this.iconMenuItem1.AutoSize = false;
+            this.iconMenuItem1.BackColor = System.Drawing.Color.Honeydew;
+            this.iconMenuItem1.IconChar = FontAwesome.Sharp.IconChar.UserPlus;
+            this.iconMenuItem1.IconColor = System.Drawing.Color.Black;
+            this.iconMenuItem1.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconMenuItem1.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.iconMenuItem1.Name = "iconMenuItem1";
+            this.iconMenuItem1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.iconMenuItem1.Size = new System.Drawing.Size(100, 75);
+            this.iconMenuItem1.Text = "Usuarios";
+            this.iconMenuItem1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.iconMenuItem1.Click += new System.EventHandler(this.iconMenuItem1_Click);
+            // 
+            // iconMenuItem2
+            // 
+            this.iconMenuItem2.AutoSize = false;
+            this.iconMenuItem2.BackColor = System.Drawing.Color.Honeydew;
+            this.iconMenuItem2.IconChar = FontAwesome.Sharp.IconChar.Tornado;
+            this.iconMenuItem2.IconColor = System.Drawing.Color.Black;
+            this.iconMenuItem2.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconMenuItem2.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.iconMenuItem2.Name = "iconMenuItem2";
+            this.iconMenuItem2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.iconMenuItem2.Size = new System.Drawing.Size(100, 75);
+            this.iconMenuItem2.Text = "Mantenimientos";
+            this.iconMenuItem2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.iconMenuItem2.Click += new System.EventHandler(this.iconMenuItem2_Click);
+            // 
+            // iconMenuItem4
+            // 
+            this.iconMenuItem4.AutoSize = false;
+            this.iconMenuItem4.BackColor = System.Drawing.Color.Honeydew;
+            this.iconMenuItem4.IconChar = FontAwesome.Sharp.IconChar.Tags;
+            this.iconMenuItem4.IconColor = System.Drawing.Color.Black;
+            this.iconMenuItem4.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconMenuItem4.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.iconMenuItem4.Name = "iconMenuItem4";
+            this.iconMenuItem4.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.iconMenuItem4.Size = new System.Drawing.Size(100, 75);
+            this.iconMenuItem4.Text = "Ventas";
+            this.iconMenuItem4.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // iconMenuItem5
+            // 
+            this.iconMenuItem5.AutoSize = false;
+            this.iconMenuItem5.BackColor = System.Drawing.Color.Honeydew;
+            this.iconMenuItem5.IconChar = FontAwesome.Sharp.IconChar.UserGroup;
+            this.iconMenuItem5.IconColor = System.Drawing.Color.Black;
+            this.iconMenuItem5.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconMenuItem5.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.iconMenuItem5.Name = "iconMenuItem5";
+            this.iconMenuItem5.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.iconMenuItem5.Size = new System.Drawing.Size(100, 75);
+            this.iconMenuItem5.Text = "Clientes";
+            this.iconMenuItem5.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // iconMenuItem6
+            // 
+            this.iconMenuItem6.AutoSize = false;
+            this.iconMenuItem6.BackColor = System.Drawing.Color.Honeydew;
+            this.iconMenuItem6.IconChar = FontAwesome.Sharp.IconChar.Tents;
+            this.iconMenuItem6.IconColor = System.Drawing.Color.Black;
+            this.iconMenuItem6.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconMenuItem6.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.iconMenuItem6.Name = "iconMenuItem6";
+            this.iconMenuItem6.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.iconMenuItem6.Size = new System.Drawing.Size(100, 75);
+            this.iconMenuItem6.Text = "Proveedores";
+            this.iconMenuItem6.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // iconMenuItem7
+            // 
+            this.iconMenuItem7.AutoSize = false;
+            this.iconMenuItem7.BackColor = System.Drawing.Color.Honeydew;
+            this.iconMenuItem7.IconChar = FontAwesome.Sharp.IconChar.Trash;
+            this.iconMenuItem7.IconColor = System.Drawing.Color.Black;
+            this.iconMenuItem7.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconMenuItem7.Name = "iconMenuItem7";
+            this.iconMenuItem7.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.iconMenuItem7.Size = new System.Drawing.Size(100, 75);
+            this.iconMenuItem7.Text = "Reportes";
+            this.iconMenuItem7.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // menuIconos
+            // 
+            this.menuIconos.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.menuIconos.Location = new System.Drawing.Point(0, 0);
+            this.menuIconos.Name = "menuIconos";
+            this.menuIconos.Size = new System.Drawing.Size(714, 24);
+            this.menuIconos.TabIndex = 1;
+            this.menuIconos.Text = "menuIconos";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.Font = new System.Drawing.Font("Lucida Calligraphy", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label1.Location = new System.Drawing.Point(156, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(387, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Sistema de Compras y Ventas POS";
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(106, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(608, 420);
+            this.panel1.TabIndex = 3;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // frmInicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(714, 444);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuTitulo);
+            this.Controls.Add(this.menuIconos);
+            this.MainMenuStrip = this.menuTitulo;
+            this.Name = "frmInicio";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.Text = "Sistema de Ventas POS";
+            this.menuTitulo.ResumeLayout(false);
+            this.menuTitulo.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuTitulo;
+        private System.Windows.Forms.MenuStrip menuIconos;
+        private FontAwesome.Sharp.IconMenuItem iconMenuItem1;
+        private System.Windows.Forms.Label label1;
+        private FontAwesome.Sharp.IconMenuItem iconMenuItem7;
+        private FontAwesome.Sharp.IconMenuItem iconMenuItem6;
+        private FontAwesome.Sharp.IconMenuItem iconMenuItem5;
+        private FontAwesome.Sharp.IconMenuItem iconMenuItem4;
+        private FontAwesome.Sharp.IconMenuItem iconMenuItem3;
+        private FontAwesome.Sharp.IconMenuItem iconMenuItem2;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ CapaPresentacion/Login.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace CapaPresentacion
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblLogoEmpresa = new System.Windows.Forms.Label();
+            this.iconPictureBox1 = new FontAwesome.Sharp.IconPictureBox();
+            this.lblTituloLogin = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtContraseña = new System.Windows.Forms.Label();
+            this.iconAceptar = new FontAwesome.Sharp.IconButton();
+            this.iconCancelar = new FontAwesome.Sharp.IconButton();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblLogoEmpresa
+            // 
+            this.lblLogoEmpresa.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.lblLogoEmpresa.Location = new System.Drawing.Point(-3, -2);
+            this.lblLogoEmpresa.Name = "lblLogoEmpresa";
+            this.lblLogoEmpresa.Size = new System.Drawing.Size(264, 317);
+            this.lblLogoEmpresa.TabIndex = 0;
+            // 
+            // iconPictureBox1
+            // 
+            this.iconPictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.iconPictureBox1.ForeColor = System.Drawing.SystemColors.Control;
+            this.iconPictureBox1.IconChar = FontAwesome.Sharp.IconChar.Safari;
+            this.iconPictureBox1.IconColor = System.Drawing.SystemColors.Control;
+            this.iconPictureBox1.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconPictureBox1.IconSize = 83;
+            this.iconPictureBox1.Location = new System.Drawing.Point(74, 112);
+            this.iconPictureBox1.Name = "iconPictureBox1";
+            this.iconPictureBox1.Size = new System.Drawing.Size(83, 83);
+            this.iconPictureBox1.TabIndex = 1;
+            this.iconPictureBox1.TabStop = false;
+            this.iconPictureBox1.Click += new System.EventHandler(this.iconPictureBox1_Click);
+            // 
+            // lblTituloLogin
+            // 
+            this.lblTituloLogin.AutoSize = true;
+            this.lblTituloLogin.BackColor = System.Drawing.Color.RosyBrown;
+            this.lblTituloLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.lblTituloLogin.Location = new System.Drawing.Point(48, 212);
+            this.lblTituloLogin.Name = "lblTituloLogin";
+            this.lblTituloLogin.Size = new System.Drawing.Size(139, 25);
+            this.lblTituloLogin.TabIndex = 2;
+            this.lblTituloLogin.Text = "Sistema POS";
+            this.lblTituloLogin.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(292, 58);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(235, 20);
+            this.txtUsuario.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(292, 119);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
+            this.textBox2.Size = new System.Drawing.Size(235, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.label1.Location = new System.Drawing.Point(289, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Nombre Usuario";
+            // 
+            // txtContraseña
+            // 
+            this.txtContraseña.AutoSize = true;
+            this.txtContraseña.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.txtContraseña.Location = new System.Drawing.Point(289, 96);
+            this.txtContraseña.Name = "txtContraseña";
+            this.txtContraseña.Size = new System.Drawing.Size(95, 20);
+            this.txtContraseña.TabIndex = 6;
+            this.txtContraseña.Text = "Contraseña";
+            // 
+            // iconAceptar
+            // 
+            this.iconAceptar.IconChar = FontAwesome.Sharp.IconChar.None;
+            this.iconAceptar.IconColor = System.Drawing.Color.Black;
+            this.iconAceptar.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconAceptar.Location = new System.Drawing.Point(292, 172);
+            this.iconAceptar.Name = "iconAceptar";
+            this.iconAceptar.Size = new System.Drawing.Size(75, 23);
+            this.iconAceptar.TabIndex = 7;
+            this.iconAceptar.Text = "Aceptar";
+            this.iconAceptar.UseVisualStyleBackColor = true;
+            this.iconAceptar.Click += new System.EventHandler(this.iconAceptar_Click);
+            // 
+            // iconCancelar
+            // 
+            this.iconCancelar.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.iconCancelar.IconChar = FontAwesome.Sharp.IconChar.None;
+            this.iconCancelar.IconColor = System.Drawing.Color.Black;
+            this.iconCancelar.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconCancelar.Location = new System.Drawing.Point(452, 172);
+            this.iconCancelar.Name = "iconCancelar";
+            this.iconCancelar.Size = new System.Drawing.Size(75, 23);
+            this.iconCancelar.TabIndex = 8;
+            this.iconCancelar.Text = "Cancelar";
+            this.iconCancelar.UseVisualStyleBackColor = true;
+            this.iconCancelar.Click += new System.EventHandler(this.iconButton2_Click);
+            // 
+            // frmLogin
+            // 
+            this.AcceptButton = this.iconAceptar;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.iconCancelar;
+            this.ClientSize = new System.Drawing.Size(567, 316);
+            this.Controls.Add(this.iconCancelar);
+            this.Controls.Add(this.iconAceptar);
+            this.Controls.Add(this.txtContraseña);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.lblTituloLogin);
+            this.Controls.Add(this.iconPictureBox1);
+            this.Controls.Add(this.lblLogoEmpresa);
+            this.Name = "frmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Iniciar Sesion";
+            this.Load += new System.EventHandler(this.frmLogin_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblLogoEmpresa;
+        private FontAwesome.Sharp.IconPictureBox iconPictureBox1;
+        private System.Windows.Forms.Label lblTituloLogin;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label txtContraseña;
+        private FontAwesome.Sharp.IconButton iconAceptar;
+        private FontAwesome.Sharp.IconButton iconCancelar;
+    }
+}
